db07f49b5ae1b7130e4d625dbe951b8b58cbd487
saulordz@gmail.com <saulordz@gmail.com>
2016-04-25 09:27:03 -0400

    First commit

---
+++ app/src/main/java/saulo/com/w5d1_leak_cannary/MainActivity.java
@@ -0,0 +1,13 @@
+package saulo.com.w5d1_leak_cannary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
